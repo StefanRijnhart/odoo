08188a45d91a0ebd3eb8762a0ffee66623c9c5ef
ronald@therp.nl <ronald@therp.nl>
2014-10-29 11:23:32 +0100

    [FIX] lp:754339, Partner assigned Price List is not applied with the creation of a customer/supplier invoice

--- addons/account/tests/__init__.py
+++ addons/account/tests/__init__.py
@@ -1,7 +1,9 @@
 from . import test_tax
+from . import test_product_id_change
 from . import test_search
 
 fast_suite = [
-	test_tax,
-	test_search,
+        test_tax,
+        test_product_id_change,
+        test_search,
 ]
