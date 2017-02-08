e794413526b98ac4b7ccf6c7bea3d6bb17cd973d
Stefan Rijnhart <stefan@opener.am>
2017-02-08 21:44:38 +0100

    [RVT] Backported fixes in buckaroo attribute casing; URLs and return code in custom module; noupdate modified view

--- addons/payment_buckaroo/controllers/main.py
+++ addons/payment_buckaroo/controllers/main.py
@@ -15,10 +15,10 @@ _logger = logging.getLogger(__name__)
 
 
 class BuckarooController(http.Controller):
-    _return_url = 'https://www.roetz-bikes.com/payment/buckaroo/return'
-    _cancel_url = 'https://www.roetz-bikes.com/payment/buckaroo/cancel'
-    _exception_url = 'https://www.roetz-bikes.com/payment/buckaroo/error'
-    _reject_url = 'https://www.roetz-bikes.com/payment/buckaroo/reject'
+    _return_url = '/payment/buckaroo/return'
+    _cancel_url = '/payment/buckaroo/cancel'
+    _exception_url = '/payment/buckaroo/error'
+    _reject_url = '/payment/buckaroo/reject'
 
     @http.route([
         '/payment/buckaroo/return',
@@ -30,15 +30,9 @@ class BuckarooController(http.Controller):
         """ Buckaroo."""
         _logger.info('Buckaroo: entering form_feedback with post data %s', pprint.pformat(post))  # debug
         request.registry['payment.transaction'].form_feedback(request.cr, SUPERUSER_ID, post, 'buckaroo', context=request.context)
-        if post.get('brq_statuscode') in ('190', '790', '791', '792', '793'):
-            return_url = '/shop/payment/validate'        
-        else:
-            return_url = '/page/payment-failed'
-        #return_url = post.pop('return_url', '')
-        #_logger.info(return_url)
-        #if not return_url:
-        #    data ='' + post.pop('ADD_RETURNDATA', '{}').replace("'", "\"")
-        #    custom = json.loads(data)
-        #    return_url = custom.pop('return_url', '/')
-        #    _logger.info(return_url)
+        return_url = post.pop('return_url', '')
+        if not return_url:
+            data ='' + post.pop('ADD_RETURNDATA', '{}').replace("'", "\"")
+            custom = json.loads(data)
+            return_url = custom.pop('return_url', '/')
         return werkzeug.utils.redirect(return_url)
