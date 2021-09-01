955a072b4d88af7536ced41239fbbb828fa52d6a
Holger Brunn <mail@hunki-enterprises.com>
2021-09-01 07:44:29 +0200

    [ADD] IS_OP opcode

--- odoo/tools/safe_eval.py
+++ odoo/tools/safe_eval.py
@@ -92,6 +92,8 @@ _EXPR_OPCODES = _CONST_OPCODES.union(set(opmap[x] for x in [
     # specialised comparisons
     'CONTAINS_OP',
     'DICT_MERGE',
+    # py39
+    'IS_OP',
 ] if x in opmap))
 
 _SAFE_OPCODES = _EXPR_OPCODES.union(set(opmap[x] for x in [
